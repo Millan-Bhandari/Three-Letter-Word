9fd401d0c7c4c7e8eb069b7524dd98aaaa03e2f6
Millan Bhandari <millan.ib@icloud.com>
2022-07-22 09:52:36 -0500

    Initial Commit

---
+++ Three Letter Word/Three_Letter_WordApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_Letter_WordApp.swift
+//  Three Letter Word
+//
+//  Created by Millan Bhandari on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Three_Letter_WordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
